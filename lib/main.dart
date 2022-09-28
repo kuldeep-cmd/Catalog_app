55d423e3d4342a99395c20984b7834f7713d1e3c
kuldeep <kuldeepnath0@gmail.com>
2022-09-28 09:34:23 +0530

    Day2

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,16 @@
+import 'package:catagory_apps/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({ Key key }) : super(key: key);
-
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("welcome to my 30days course of flutter") ,
-            ),
-        ),
-      ),
-    ) ;
-
-      
-    
+      home: HomePage(),
+    );
   }
 }
